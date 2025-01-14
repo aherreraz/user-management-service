d7ebf7b98d0a8644ed0b53b7167faa5fb06e838e
Andres Herrera <aherrera@bevertec.com>
2025-01-13 23:55:52 -0500

    test: add tests for user domain class

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -8,3 +8,13 @@ java {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+    testImplementation("org.mockito:mockito-core:3.+")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ domain/src/test/java/com/aherreraz/usermanagement/model/UserTest.java
@@ -0,0 +1,47 @@
+package com.aherreraz.usermanagement.model;
+
+import com.aherreraz.usermanagement.port.PasswordEncoderPort;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class UserTest {
+    @Test
+    @DisplayName("Should create a User with default values")
+    void shouldCreateUser() {
+        User newUser = new User();
+
+        assertAll(
+                () -> assertInstanceOf(UUID.class, newUser.getId()),
+                () -> assertInstanceOf(LocalDateTime.class, newUser.getCreated()),
+                () -> assertNull(newUser.getLastLogin()),
+                () -> assertEquals(newUser.getIsActive(), true),
+                () -> assertNull(newUser.getEmail()),
+                () -> assertNull(newUser.getName()),
+                () -> assertNull(newUser.getPassword()),
+                () -> assertNull(newUser.getPhones())
+        );
+    }
+
+    @Test
+    @DisplayName("Should encode password using password encoder")
+    void shouldEncodePassword() {
+        PasswordEncoderPort passwordEncoder = mock(PasswordEncoderPort.class);
+        when(passwordEncoder.encode("cleanPassword")).thenReturn("encodedPassword");
+
+        User newUser = new User();
+        newUser.setCleanPassword("cleanPassword");
+        newUser.encodePassword(passwordEncoder);
+
+        assertEquals(newUser.getPassword(), "encodedPassword");
+    }
+}
