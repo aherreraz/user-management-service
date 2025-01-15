2ac9565082eef5c5f11830f0e02200956778d4d7
Andres Herrera <aherrera@bevertec.com>
2025-01-14 20:59:55 -0500

    chore: organize imports in application module

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     java
     id("io.freefair.lombok") version "8.11"
-    id("org.springframework.boot") version "2.5.14"
-    id("io.spring.dependency-management") version "1.1.7"
 }
 
 java {
@@ -12,12 +10,20 @@ java {
 }
 
 dependencies {
+    // Module Dependencies
     implementation(project(":domain"))
+
+    // Spring
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.5.14"))
     implementation("org.springframework:spring-context")
     implementation("org.hibernate.validator:hibernate-validator")
+
+    // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    // Mapstruct
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 }
